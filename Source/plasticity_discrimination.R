ed2b9f839e4a5df8d81c6316575ca5a3a415b97f
callum-lawson <callumrlawson@gmail.com>
2017-09-03 19:21:26 +0200

    Add mean change for high-variance scenario.

--- Source/plasticity_discrimination.R
+++ Source/plasticity_discrimination.R
@@ -6,7 +6,6 @@ require(MASS)
 
 zwdist <- function(zmu,wmu,zsd,wsd,rho=0.8,nsim=10^6,...){
   zw_mu <- c(zmu,wmu)
-  set.seed(1)
   zw_sig <- matrix(c(zsd^2,rep(rho*zsd*wsd,2),wsd^2),nr=2,nc=2)
   zw <- mvrnorm(n=nsim, mu=zw_mu, Sigma=zw_sig)
   colnames(zw) <- c("z","w")
@@ -22,12 +21,14 @@ mybreaks <- c(-1.05,-0.95,-0.05,0.05,0.95,1.05)
 
 # Imperfect information ---------------------------------------------------
 
+set.seed(100)
 zw1 <- zwdist(zmu=0,wmu=0,zsd=0.1,wsd=1,breaks=mybreaks)
 zw2 <- zwdist(zmu=0,wmu=0,zsd=0.1*2,wsd=1*2,breaks=mybreaks)
-zw3 <- zwdist(zmu=-0.1,wmu=-1,zsd=0.1,wsd=1,breaks=mybreaks)
+zw3 <- zwdist(zmu=-0.2,wmu=-2,zsd=0.1,wsd=1,breaks=mybreaks)
+zw4 <- zwdist(zmu=-0.2,wmu=-2,zsd=0.1*2,wsd=1*2,breaks=mybreaks)
 
 par(mfrow=c(1,1))
-plot(density(zw1$byw[[1]]),lty=1,col="black")
+plot(density(zw1$byw[[1]]),lty=1,col="black",main="")
 lines(density(zw1$byw[[3]]),lty=2,col="black")
 lines(density(zw1$byw[[5]]),lty=3,col="black")
 lines(density(zw2$byw[[1]]),lty=1,col="blue")
@@ -36,11 +37,15 @@ lines(density(zw2$byw[[5]]),lty=3,col="blue")
 lines(density(zw3$byw[[1]]),lty=1,col="red")
 lines(density(zw3$byw[[3]]),lty=2,col="red")
 lines(density(zw3$byw[[5]]),lty=3,col="red")
+lines(density(zw4$byw[[1]]),lty=1,col="orange")
+lines(density(zw4$byw[[3]]),lty=2,col="orange")
+lines(density(zw4$byw[[5]]),lty=3,col="orange")
 # Increasing zsd only scales z up and down - doesn't change discrimination ability
 
 plot(z~w,data=zw2$zw[1:1000,],col="blue")
 points(z~w,data=zw1$zw[1:1000,],col="black")
 points(z~w,data=zw3$zw[1:1000,],col="red")
+points(z~w,data=zw4$zw[1:1000,],col="orange")
 
 # Perfect information -----------------------------------------------------
 
